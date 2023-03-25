020d52f02c20057df2367b9c9e0d67126d7e160b
m_amirarsalan@outlook.com <m_amirarsalan@outlook.com>
2023-03-25 10:26:42 +0430

    Add: Generic Repository Interface and Implementation

---
+++ EShop.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using EShop.Domain.Entities.Common;
+
+namespace EShop.Domain.Interfaces
+{
+    public interface IGenericRepository<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetEntitiesQuery();
+
+        Task<TEntity> GetEntityById(long entityId);
+
+        Task AddEntity(TEntity entity);
+
+        void UpdateEntity(TEntity entity);
+
+        void RemoveEntity(TEntity entity);
+        Task RemoveEntity(long entityId);
+        Task SaveChanges();
+    }
+}
---
+++ EShop.Infrastructure.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using EShop.Domain.Entities.Common;
+using EShop.Domain.Interfaces;
+using EShop.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Infrastructure.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        private EShopDbContext _context;
+        private DbSet<TEntity> _dbSet;
+
+        #region Constructor
+
+        public GenericRepository(EShopDbContext dbContext)
+        {
+            _context = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        #endregion
+
+        public IQueryable<TEntity> GetEntitiesQuery()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public async Task<TEntity> GetEntityById(long entityId)
+        {
+            return await _dbSet.SingleOrDefaultAsync(e => e.Id == entityId);
+        }
+
+        public async Task AddEntity(TEntity entity)
+        {
+            entity.CreateDate = DateTime.Now;
+            entity.LastUpdateDate = entity.CreateDate;
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void UpdateEntity(TEntity entity)
+        {
+            entity.LastUpdateDate = DateTime.Now;
+            _dbSet.Update(entity);
+        }
+
+        public void RemoveEntity(TEntity entity)
+        {
+            entity.IsDeleted = true;
+            entity.LastUpdateDate= DateTime.Now;
+            _dbSet.Update(entity); 
+        }
+
+        public async Task RemoveEntity(long entityId)
+        {
+            var entity = await GetEntityById(entityId);
+            RemoveEntity(entity);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+
+    }
+}
